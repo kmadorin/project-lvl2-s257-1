a3d45393a5285d039ee4110c705e79d393b3332b
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-15 13:53:02 +0300

    fix version output

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import program from 'commander';
 
-const pkgVersion = process.env.npm_package_version;
-
 export default () => {
   program
-    .version(`${pkgVersion}`)
+    .version('0.1.0')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
