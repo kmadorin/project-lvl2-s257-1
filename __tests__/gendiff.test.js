c4f6e04f15f862de82f9be3a3109d6072e1ce8f1
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-16 09:51:38 +0300

    add tests for reverse difference

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,3 +7,10 @@ test('difference beetwen two JSON files', () => {
   const diff = fs.readFileSync('__tests__/__fixtures__/difference', 'utf-8');
   expect(genDiff(firstConfigPath, secondConfigPath)).toBe(diff);
 });
+
+test('reverse difference beetwen two JSON files', () => {
+  const firstConfigPath = fs.realpathSync('__tests__/__fixtures__/after.json');
+  const secondConfigPath = fs.realpathSync('__tests__/__fixtures__/before.json');
+  const diff = fs.readFileSync('__tests__/__fixtures__/differenceReverse', 'utf-8');
+  expect(genDiff(firstConfigPath, secondConfigPath)).toBe(diff);
+});
