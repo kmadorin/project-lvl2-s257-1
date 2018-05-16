e078bf8770e2e7acee3bda44d42c530c0fe8b99b
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-16 13:45:08 +0300

    add tests for YAML comparison

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,11 +2,19 @@ import fs from 'fs';
 import genDiff from '../src';
 
 test('difference beetwen two JSON files', () => {
-  const firstConfigPath = fs.realpathSync('__tests__/__fixtures__/before.json');
-  const secondConfigPath = fs.realpathSync('__tests__/__fixtures__/after.json');
+  const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.json');
+  const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.json');
   const diff = fs.readFileSync('__tests__/__fixtures__/difference', 'utf-8');
   const diffReverse = fs.readFileSync('__tests__/__fixtures__/differenceReverse', 'utf-8');
-  expect(genDiff(firstConfigPath, secondConfigPath)).toBe(diff);
-  expect(genDiff(secondConfigPath, firstConfigPath)).toBe(diffReverse);
+  expect(genDiff(configPath1, configPath2)).toBe(diff);
+  expect(genDiff(configPath2, configPath1)).toBe(diffReverse);
 });
 
+test('difference beetwen two YML files', () => {
+  const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.yml');
+  const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.yml');
+  const diff = fs.readFileSync('__tests__/__fixtures__/difference', 'utf-8');
+  const diffReverse = fs.readFileSync('__tests__/__fixtures__/differenceReverse', 'utf-8');
+  expect(genDiff(configPath1, configPath2)).toBe(diff);
+  expect(genDiff(configPath2, configPath1)).toBe(diffReverse);
+});
