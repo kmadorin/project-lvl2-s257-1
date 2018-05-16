507ba371b36a7dc33c6b36cc9ed30710d83538b9
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-16 09:50:21 +0300

    fix diff output

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import _ from 'lodash';
 const compareObjects = (firstObject, secondObject) => {
   const firstObjectChanges = _.keys(firstObject).reduce((acc, key) => {
     if (!_.has(secondObject, key)) {
-      return `${acc}\n  - ${key}: ${firstObject[key]}`;
+      return `${acc}  - ${key}: ${firstObject[key]}\n`;
     }
     if (secondObject[key] !== firstObject[key]) {
-      return `${acc}\n  + ${key}: ${secondObject[key]}\n  - ${key}: ${firstObject[key]}`;
+      return `${acc}  + ${key}: ${secondObject[key]}\n  - ${key}: ${firstObject[key]}\n`;
     }
-    return `${acc}    ${key}: ${firstObject[key]}`;
+    return `${acc}    ${key}: ${firstObject[key]}\n`;
   }, '');
 
   const differenceString = _.keys(secondObject).reduce((acc, key) => {
     if (!_.has(firstObject, key)) {
-      return `${acc}\n  + ${key}: ${secondObject[key]}`;
+      return `${acc}  + ${key}: ${secondObject[key]}\n`;
     }
     return acc;
   }, firstObjectChanges);
@@ -27,7 +27,7 @@ const genDiff = (firstConfigPath, secondConfigPath) => {
   const secondConfig = fs.readFileSync(secondConfigPath, 'utf-8');
   const firstObject = JSON.parse(firstConfig);
   const secondObject = JSON.parse(secondConfig);
-  return `{\n${compareObjects(firstObject, secondObject)}\n}`;
+  return `{\n${compareObjects(firstObject, secondObject)}}`;
 };
 
 export default genDiff;
