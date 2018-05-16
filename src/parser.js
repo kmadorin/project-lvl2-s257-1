702729f26c64f10364639d5c716f8e0f68b4ed0c
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-16 14:11:39 +0300

    INI comparison is implemented

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,10 @@
 import { safeLoad } from 'js-yaml';
+import { parse as iniParse } from 'ini';
 
 const parsers = {
   '.json': JSON.parse,
   '.yml': safeLoad,
+  '.ini': iniParse,
 };
 
 export default extension => data => parsers[extension](data);
