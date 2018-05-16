56df4c419fa755cf033b87070c0fd82ca865cbb5
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-16 09:33:42 +0300

    update version

--- src/cli/index.js
+++ src/cli/index.js
@@ -3,7 +3,7 @@ import genDiff from '..';
 
 export default () => {
   program
-    .version('0.1.1')
+    .version('0.2.0')
     .arguments('<firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
