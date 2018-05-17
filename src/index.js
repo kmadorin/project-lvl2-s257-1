a2c3574eac07cf19a93fcf86970491e16d1513c8
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-17 17:12:18 +0300

    nested differencies is implemented

--- src/index.js
+++ src/index.js
@@ -3,20 +3,42 @@ import { extname } from 'path';
 import _ from 'lodash';
 import getParser from './parser';
 
-const compareObjects = (object1, object2) => {
-  const keys = _.union(_.keys(object1), _.keys(object2));
-  const differences = keys.map((key) => {
-    if (!_.has(object1, key)) {
-      return `  + ${key}: ${object2[key]}`;
-    }
-    if (!_.has(object2, key)) {
-      return `  - ${key}: ${object1[key]}`;
-    }
-    const changes = object2[key] !== object1[key] ? `+ ${key}: ${object2[key]}\n  -` : ' ';
-    return `  ${changes} ${key}: ${object1[key]}`;
-  });
+// {
+//   keyName: ['name', '+/-/', 'str'/'[children]]',
+// }
 
-  return differences.join('\n');
+const diffAst = (object1, object2) => {
+  const keys = _.union(_.keys(object1), _.keys(object2));
+  const ast = keys.reduce((acc, key) => {
+    if (!_.has(object2, key)) {
+      return [...acc, [key, '-', object1[key]]];
+    }
+    if (!_.has(object1, key)) {
+      return [...acc, [key, '+', object2[key]]];
+    }
+    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+      return [...acc, [key, '', diffAst(object1[key], object2[key])]];
+    }
+    if (object1[key] !== object2[key]) {
+      return [...acc, [key, '-', object1[key]], [key, '+', object2[key]]];
+    }
+    return [...acc, [key, '', object1[key]]];
+  }, []);
+  return ast;
+};
+
+const renderDiff = (ast, spacesNum = 2) => {
+  const valueToString = (val) => {
+    if (_.isArray(val)) {
+      return `{\n${renderDiff(val, spacesNum + 4)}\n${' '.repeat(spacesNum)}  }`;
+    }
+    if (_.isObject(val)) {
+      return `{\n${_.keys(val).map(key => `${' '.repeat(spacesNum + 4)}  ${key}: ${val[key]}`).join('\n')}\n${' '.repeat(spacesNum)}  }`;
+    }
+    return val;
+  };
+  const res = ast.map(el => `${' '.repeat(spacesNum)}${el[1] || ' '} ${el[0]}: ${valueToString(el[2])}`);
+  return res.join('\n');
 };
 
 const genDiff = (configPath1, configPath2) => {
@@ -26,7 +48,7 @@ const genDiff = (configPath1, configPath2) => {
   const configData2 = readFileSync(configPath2, 'utf-8');
   const object1 = parseData(configData1);
   const object2 = parseData(configData2);
-  return `{\n${compareObjects(object1, object2)}\n}`;
+  return `{\n${renderDiff(diffAst(object1, object2))}\n}`;
 };
 
 export default genDiff;
