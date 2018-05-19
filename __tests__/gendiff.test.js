45aa1ef9c70a46d39383c08ffe27c3270259e76e
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-19 14:59:41 +0300

    add tests for plain format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -28,6 +28,15 @@ test('difference beetwen two INI files', () => {
   expect(genDiff(configPath2, configPath1)).toBe(diffReverse);
 });
 
+test('difference beetwen two JSON files in plain format', () => {
+  const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.JSON');
+  const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.json');
+  const diff = fs.readFileSync('__tests__/__fixtures__/plainDiff', 'utf-8');
+  const diffReverse = fs.readFileSync('__tests__/__fixtures__/plainDiffReverse', 'utf-8');
+  expect(genDiff(configPath1, configPath2, 'plain')).toBe(diff);
+  expect(genDiff(configPath2, configPath1, 'plain')).toBe(diffReverse);
+});
+
 test('difference beetwen two incorrect type files', () => {
   const configPath1 = fs.realpathSync('__tests__/__fixtures__/before.abc');
   const configPath2 = fs.realpathSync('__tests__/__fixtures__/after.ini');
