bd8c1591f2babbc18bcfa15f702273493ff4fb8e
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-19 17:38:13 +0300

    json output format is implemented

--- src/render/index.js
+++ src/render/index.js
@@ -1,9 +1,11 @@
 import plainRender from './plain';
 import defaultRender from './default';
+import jsonRender from './json';
 
 const renders = {
   default: defaultRender,
   plain: plainRender,
+  json: jsonRender,
 };
 
 export default renderFormat => data => renders[`${renderFormat}`](data);
