6e6c91e503e3c3511551183d8416c92a2a58af42
Dmitriy Pavlenko <azeos1101@ya.ru>
2018-05-20 11:19:51 +0300

    rewrite buildAst logic

--- src/index.js
+++ src/index.js
@@ -1,60 +1,58 @@
 import { readFileSync } from 'fs';
-import { extname } from 'path';
 import _ from 'lodash';
 import getParser from './parser';
-import renders from './renders';
+import renderers from './renderers';
 
-const buildAst = (object1, object2) => {
-  const propertyNames = _.union(_.keys(object1), _.keys(object2));
-  const ast = propertyNames.reduce((acc, name) => {
-    if (!_.has(object2, name)) {
-      return [...acc, {
-        type: 'removed',
-        name,
-        value: object1[name],
-      }];
-    }
-    if (!_.has(object1, name)) {
-      return [...acc, {
-        type: 'added',
-        name,
-        value: object2[name],
-      }];
-    }
-    if (_.isObject(object1[name]) && _.isObject(object2[name])) {
-      return [...acc, {
-        type: 'nested',
-        name,
-        children: buildAst(object1[name], object2[name]),
-      }];
-    }
-    if (object1[name] !== object2[name]) {
-      return [...acc, {
-        type: 'updated',
-        name,
-        oldValue: object1[name],
-        newValue: object2[name],
-      }];
-    }
-    return [...acc, {
-      type: 'original',
-      name,
-      value: object1[name],
-    }];
-  }, []);
-  return ast;
+const diffTypes = [
+  {
+    type: 'nested',
+    check: (config1, config2, key) =>
+      _.isObject(config1[key]) && _.isObject(config2[key]),
+    fillProps: (config1, config2, func) => ({ children: func(config1, config2) }),
+  },
+  {
+    type: 'original',
+    check: (config1, config2, key) => (_.has(config1, key) && _.has(config2, key)
+      && (config1[key] === config2[key])),
+    fillProps: config1 => ({ value: config1 }),
+  },
+  {
+    type: 'updated',
+    check: (config1, config2, key) => (_.has(config1, key) && _.has(config2, key)
+      && (config1[key] !== config2[key])),
+    fillProps: (config1, config2) =>
+      ({ oldValue: config1, newValue: config2 }),
+  },
+  {
+    type: 'removed',
+    check: (config1, config2, key) =>
+      (_.has(config1, key) && !_.has(config2, key)),
+    fillProps: config1 => ({ value: config1 }),
+  },
+  {
+    type: 'added',
+    check: (config1, config2, key) =>
+      (!_.has(config1, key) && _.has(config2, key)),
+    fillProps: (config1, config2) => ({ value: config2 }),
+  },
+];
+
+const buildAst = (firstConfig = {}, config2 = {}) => {
+  const propertyNames = _.union(_.keys(firstConfig), _.keys(config2));
+  return propertyNames.map((name) => {
+    const { type, fillProps } = _.find(diffTypes, item => item.check(firstConfig, config2, name));
+    return { type, name, ...fillProps(firstConfig[name], config2[name], buildAst) };
+  });
 };
 
-const genDiff = (configPath1, configPath2, renderType = 'default') => {
-  const ext1 = extname(configPath1).toLowerCase();
-  const ext2 = extname(configPath2).toLowerCase();
-  const parseData1 = getParser(ext1);
-  const parseData2 = getParser(ext2);
+const genDiff = (configPath1, configPath2, renderType = 'standart') => {
+  const parseData1 = getParser(configPath1);
+  const parseData2 = getParser(configPath2);
   const configData1 = readFileSync(configPath1, 'utf-8');
   const configData2 = readFileSync(configPath2, 'utf-8');
   const object1 = parseData1(configData1);
   const object2 = parseData2(configData2);
-  const render = renders[renderType];
+  const render = renderers[renderType];
   return render(buildAst(object1, object2));
 };
 
